8a5df963aea67013d8df22b3e4deb10bd1aace66
Fadero Joshua <phadecoh99@gmail.com>
2026-09-15 17:55:34 +0100

    Update App.tsx
    added the link for LinSentry project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -55,7 +55,7 @@ const PROJECTS = [
     title: "LinSentry",
     category: "Bash . Shell . Coming Soon",
     year: "2026",
-    url: "https://github.com/phadecoh8/LinSentry",
+    url: "https://linsentry.vercel.app",
     desc: "A linux lightweight security hardening auditor, written in Bash.",
     img: "https://images.unsplash.com/photo-1640552435845-d65c23b75934?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     alt: "LinSentry",
