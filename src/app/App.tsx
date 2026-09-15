43413bdb7ae81639ca7d3a08a664403dc1edfa95
Fadero Joshua <phadecoh99@gmail.com>
2026-09-15 17:53:49 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -65,7 +65,6 @@ const PROJECTS = [
 
 const SOCIAL = [
   { label: "GitHub", icon: Github, href: "https://github.com/phadecoh8" },
-  { label: "X", icon: Twitter, href: "https://x.com/phadecoh" },
   { label: "Instagram", icon: Instagram, href: "https://www.instagram.com/phad_ecoh" },
   { label: "LinkedIn", icon: Linkedin, href: "https://www.linkedin.com/in/fadero-joshua-a474ba375/" },
 ];
